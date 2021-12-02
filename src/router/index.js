8719c79d9dd0120f2f8968a3e05308baa6c39036
Ali.Moosavi <moosavi238@gmail.com>
2021-12-02 12:46:44 +0330

    add axios

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import EmailVerify from '../views/EmailVerify.vue'
 import Home from '../views/Home.vue'
 import LoginRegister from '../views/LoginRegister.vue'
 
@@ -12,9 +13,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/login-regester',
+    path: '/login-register',
     name: 'login',
     component: LoginRegister
+  },
+  {
+    path:'/email-verify',
+    name:'emailVerify',
+    component: EmailVerify ,
   }
 ]
 
