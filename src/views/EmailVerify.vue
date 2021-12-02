62ccb19cb6a42fb6ca09e884d3b7063ff66675a6
Ali.Moosavi <moosavi238@gmail.com>
2021-12-02 17:39:46 +0330

    verify email

---
+++ src/views/EmailVerify.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="6">
+        <v-card elevation="8" tile>
+          <v-card-title class="deep-purple accent-4 white--text"
+            >Verify Your Email Address</v-card-title
+          >
+          <v-card-text class="black--text mt-2 mb-0 py-0"
+            >Before proceeding, please check your email for a verification
+            link.</v-card-text
+          >
+          <v-card-text class="my-0 py-0"
+            >If you did not receive the email click here to request
+            another</v-card-text
+          >
+          <v-card-text v-if="sended" class="green--text"
+            >{{message}}</v-card-text
+          >
+          <v-card-actions>
+            <v-btn @click="send"  >
+              send
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      sended: false,
+      message:"",
+      form: {
+        email: sessionStorage.email,
+      },
+    };
+  },
+  methods: {
+    send() {
+      axios
+        .post(
+          "https://odev.abrnoc.com/fastapi/auth/resend-email-verification-token",
+          this.form
+        )
+        .then( (respons) => {
+          this.sended = true;
+          this.message = respons.data.message;
+        })
+        .catch ((respons) => {
+          console.log(respons);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
