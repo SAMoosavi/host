c1dfbf2984d6fe14c42f9ad6bef727e00fa82300
Ali.Moosavi <moosavi238@gmail.com>
2021-12-03 15:42:23 +0330

    add router forgot..

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import EmailVerify from '../views/EmailVerify.vue'
-import Home from '../views/Home.vue'
-import LoginRegister from '../views/LoginRegister.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import EmailVerify from "../views/EmailVerify.vue";
+import Home from "../views/Home.vue";
+import LoginRegister from "../views/LoginRegister.vue";
+import ForgotPassword from "../views/ForgotPassword.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/login-register',
-    name: 'login',
-    component: LoginRegister
+    path: "/login-register",
+    name: "login",
+    component: LoginRegister,
   },
   {
-    path:'/email-verify',
-    name:'emailVerify',
-    component: EmailVerify ,
-  }
-]
+    path: "/email-verify",
+    name: "emailVerify",
+    component: EmailVerify,
+  },
+  {
+    path: "/forgot-password",
+    name: "forgotPassword",
+    component: ForgotPassword,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
