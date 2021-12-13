88896d707a2b9165a8d7677281d47db986c272c3
Ali.Moosavi <moosavi238@gmail.com>
2021-12-13 14:29:27 +0330

    okey Auth

--- src/views/LoginRegister.vue
+++ src/views/LoginRegister.vue
@@ -1,48 +1,44 @@
-
-    <template>
+<template>
   <v-app class="pa-0 ma-0">
-    <v-main dark class="grey darken-4">
-    <v-row justify="center" align="center" class="pa-0 ma-0 bg-image-auth">
-      <v-col cols="12" md="5" lg="3">
-        <v-card>
-          <v-tabs
-            v-model="tab"
-            background-color="indigo darken-3"
-            centered
-            dark
-            icons-and-text
-            class="text-uppercase"
-          >
-            <v-tabs-slider></v-tabs-slider>
+      <v-row justify="center" align="center" class="pa-0 ma-0 bg-image-auth">
+        <v-col cols="12" md="5" lg="3">
+          <v-card>
+            <v-tabs
+              v-model="tab"
+              background-color="indigo darken-3"
+              centered
+              dark
+              icons-and-text
+              class="text-uppercase"
+            >
+              <v-tabs-slider></v-tabs-slider>
 
-            <v-tab href="#tab-1">
-              login
-              <v-icon>mdi-login</v-icon>
-            </v-tab>
+              <v-tab href="#tab-1">
+                login
+                <v-icon>mdi-login</v-icon>
+              </v-tab>
 
-            <v-tab href="#tab-2">
-              register
-              <v-icon>mdi-account-plus-outline</v-icon>
-            </v-tab>
-          </v-tabs>
+              <v-tab href="#tab-2">
+                register
+                <v-icon>mdi-account-plus-outline</v-icon>
+              </v-tab>
+            </v-tabs>
 
-          <v-tabs-items v-model="tab">
-            <v-tab-item v-for="i in 2" :key="i" :value="'tab-' + i">
-              <v-card >
-                <v-card-text>
-                  <div v-if="i == 1"><login /></div>
-                  <div v-if="i == 2"><register /></div>
-                </v-card-text>
-              </v-card>
-            </v-tab-item>
-          </v-tabs-items>
-        </v-card>
-      </v-col>
-    </v-row>
-    </v-main>
+            <v-tabs-items v-model="tab">
+              <v-tab-item v-for="i in 2" :key="i" :value="'tab-' + i">
+                <v-card>
+                  <v-card-text>
+                    <div v-if="i == 1"><login /></div>
+                    <div v-if="i == 2"><register /></div>
+                  </v-card-text>
+                </v-card>
+              </v-tab-item>
+            </v-tabs-items>
+          </v-card>
+        </v-col>
+      </v-row>
   </v-app>
 </template>
-    
 
 <script>
 import Login from "./Login.vue";
@@ -59,7 +55,7 @@ export default {
 </script>
 
 <style>
-.bg-image-auth{
+.bg-image-auth {
   background: url(../assets/images/background/auth.png) center center/cover;
 }
 </style>
--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -1,67 +1,63 @@
 <template>
   <v-app>
-    <v-main dark class="grey darken-4">
-      <v-row justify="center" align="center" class="bg-image-auth">
-        <v-col cols="12" md="5" lg="3">
-          <v-card elevation="8" tile>
-            <v-form v-model="valid" @submit.prevent="submit">
-              <v-container>
-                <v-card-title
-                  class="indigo darken-3 text-uppercase white--text"
+    <v-row justify="center" align="center" class="bg-image-auth">
+      <v-col cols="12" md="5" lg="3">
+        <v-card elevation="8" tile>
+          <v-form v-model="valid" @submit.prevent="submit">
+            <v-container>
+              <v-card-title class="indigo darken-3 text-uppercase white--text">
+                Reset your password
+              </v-card-title>
+              <v-row class="px-4 pt-4">
+                <p
+                  v-if="hasError"
+                  class="red--text text--darken-4 text-capitalize"
                 >
-                  Reset your password
-                </v-card-title>
-                <v-row class="px-4 pt-4">
-                  <p
-                    v-if="hasError"
-                    class="red--text text--darken-4 text-capitalize"
+                  {{ error }}
+                  <br />
+                  <router-link
+                    to="/forgot-password"
+                    class="text-decoration-underline grey--text"
+                    >Forgot password page!</router-link
                   >
-                    {{ error }}
-                    <br />
-                    <router-link
-                      to="/forgot-password"
-                      class="text-decoration-underline grey--text"
-                      >Forgot password page!</router-link
-                    >
-                  </p>
-                  <v-col cols="12" md="6">
-                    <v-text-field
-                      v-model="form.new_password"
-                      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                      :rules="[rules.required, rules.min]"
-                      :type="show1 ? 'text' : 'password'"
-                      name="input-10-1"
-                      label="Password"
-                      hint="At least 8 characters"
-                      counter
-                      @click:append="show1 = !show1"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="6">
-                    <v-text-field
-                      v-model="form.ConfirmPassword"
-                      :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
-                      :rules="[rules.required, rules.match]"
-                      :type="show2 ? 'text' : 'password'"
-                      name="input-10-1"
-                      label="Confirm password"
-                      hint="Confirm password is match"
-                      @click:append="show2 = !show2"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12">
-                    <v-btn class="d-block" :disabled="!valid" type="submit">
-                      Send
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-form>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-main>
-  </v-app>
+                </p>
+                <v-col cols="12" md="6">
+                  <v-text-field
+                    v-model="form.new_password"
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required, rules.min]"
+                    :type="show1 ? 'text' : 'password'"
+                    name="input-10-1"
+                    label="Password"
+                    hint="At least 8 characters"
+                    counter
+                    @click:append="show1 = !show1"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" md="6">
+                  <v-text-field
+                    v-model="form.ConfirmPassword"
+                    :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required, rules.match]"
+                    :type="show2 ? 'text' : 'password'"
+                    name="input-10-1"
+                    label="Confirm password"
+                    hint="Confirm password is match"
+                    @click:append="show2 = !show2"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-btn class="d-block" :disabled="!valid" type="submit">
+                    Send
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-row></v-app
+  >
 </template>
 
 <script>
